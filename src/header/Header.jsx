81b279f6c17e62604d330216d2a9f08f01826511
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-03-26 22:44:00 +0900

    first-commit

---
+++ src/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export const Header = () => {
+	return (
+		<div className="header">
+			<div className="headerTitless">
+				<span className="headerTitleSm">React & Node</span>
+				<span className="headerTitleLg">Blog</span>
+			</div>
+			<img className="headerImg" src="" alt="" />
+		</div>
+	);
+};
