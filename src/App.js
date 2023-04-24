3fc442c3943e2f70bcbbaa7f4ba6e510ba5347de
monolith0220 <funky.atmos.worlddd@gmail.com>
2023-04-24 22:45:13 +0900

    react-router実装

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import { Write } from "./pages/write/Write";
 import { Settings } from "./pages/settings/Settings";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
-		<>
+		<Router>
 			<TopBar />
-			<Register />
-		</>
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route index path="/register" element={<Register />}></Route>
+				<Route path="/login" element={<Login />}></Route>
+				<Route path="/write" element={<Write />}></Route>
+				<Route path="/settings" element={<Settings />}></Route>
+				<Route path="/post/:postId" element={<Single />}></Route>
+			</Routes>
+		</Router>
 	);
 }
 
